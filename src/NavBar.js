69d3148d5ada125d6812d0bf2e18257391ce5ffd
Shruthi Sridhar <shruthisridhar96@gmail.com>
2022-02-01 00:10:47 +0530

    CRUD with Admin Panel V2

--- src/NavBar.js
+++ src/NavBar.js
@@ -22,52 +22,6 @@ import { useHistory } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import {users} from './App.js';
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(3),
-    width: 'auto',
-  }
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  backgroundColor:'blue',
-  width:'10px',
-  border:'1px solid lightgrey'
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  '.css-1hcuvns-MuiInputBase-root .MuiInputBase-input':{
-    color:'black'
-  },
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    border:'1px solid lightgrey',
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
-}));
-
-
 export default function PrimarySearchAppBar() {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
